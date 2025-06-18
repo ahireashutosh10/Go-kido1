48c3769f3745b4d9ec8be94ad2957950a20ccceb
Ashutosh Ahire <ahireashutosh10@gmail.com>
2025-06-18 15:18:06 +0530

    Changes

--- src/pages/config.js
+++ src/pages/config.js
@@ -4,4 +4,4 @@ const isProduction = import.meta.env.MODE === 'production';
 
 export const API_BASE_URL = isProduction
   ? 'https://testapp.gokidogo.com/webapi'
-  : '/api';
+  : 'http://localhost:5173/api'; 
